2a43eed4310bb27475e50bb1d4fa2793838cec47
Paweł Kukliński <paffcio314@gmail.com>
2020-09-29 08:43:52 +0200

    Add model drafts for recruiter app

--- recruit/models.py
+++ recruit/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Recruiter(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    first_name = models.CharField(max_length=50, null=False)
+    last_name = models.CharField(max_length=50, null=False)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Candidate(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    first_name = models.CharField(max_length=50, null=False)
+    last_name = models.CharField(max_length=50, null=False)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Task(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=100, null=False)
+
+    def __str__(self):
+        return f"{self.title}"
+
+
+class Grade(models.Model):
+    """Grading on the scale described in the Dreyfus model."""
+
+    class GradeChoices(models.IntegerChoices):
+        NOVICE = (1, "Novice")
+        ADV_BEGINNER = (2, "Advanced beginner")
+        COMPETENT = (3, "Competent")
+        PROFICIENT = (4, "Proficient")
+        EXPERT = (5, "Expert")
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    value = models.IntegerField(choices=GradeChoices.choices)
+    recruiter = models.ForeignKey(Recruiter, on_delete=models.CASCADE)
+    candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.candidate} graded with {self.value} by {self.recruiter} on {self.task}"

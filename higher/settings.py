9e0f49c2cf4ed172afb8aa1a8ed812b49d48123f
Paweł Kukliński <paffcio314@gmail.com>
2020-09-30 20:11:19 +0200

    Add whitenoise conf from docs

--- higher/settings.py
+++ higher/settings.py
@@ -129,8 +129,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = "/static/" if DEBUG else "/staticfiles/"
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
+
 if ENVIRONMENT == "production":
     SECURE_BROWSER_XSS_FILTER = True
     X_FRAME_OPTIONS = "DENY"
